8ad93a30c642743843939727c415a7c353150343
TitasBSP <titasbsp@gmail.com>
2025-04-02 09:11:41 +0200

    main screen!!!!!!!!!!!!

--- MastermindC.c
+++ MastermindC.c
@@ -1,48 +1,43 @@
-/*******************************************************************************************
-*
-*   raylib [core] example - Basic window
-*
-*   Welcome to raylib!
-*
-*   To test examples, just press F6 and execute 'raylib_compile_execute' script
-*   Note that compiled executable is placed in the same folder as .c file
-*
-*   To test the examples on Web, press F6 and execute 'raylib_compile_execute_web' script
-*   Web version of the program is generated in the same folder as .c file
-*
-*   You can find all basic examples on C:\raylib\raylib\examples folder or
-*   raylib official webpage: www.raylib.com
-*
-*   Enjoy using raylib. :)
-*
-*   Example originally created with raylib 1.0, last time updated with raylib 1.0
-*
-*   Example licensed under an unmodified zlib/libpng license, which is an OSI-certified,
-*   BSD-like license that allows static linking with closed source software
-*
-*   Copyright (c) 2013-2024 Ramon Santamaria (@raysan5)
-*
-********************************************************************************************/
-
 #include "raylib.h"
+#include "stdio.h"
+#include "string.h"
+#include "stdlib.h"
 
-char Title[10] = "Mastermind";
-Vector2 posTitle = {275, 100};
-float alphaTitle = 0.0f;
+float alphaMain = 0.0f;
 
-// Positions
-
-
-//------------------------------------------------------------------------------------
-// Program main entry point
-//------------------------------------------------------------------------------------
 int main(void)
 {
-    // Initialization
+    bool mainScreen = true;
+    
+    // ----------------------- TITLE SCREEN ----------------------- //
+    
+    char Title[11] = "Mastermind";
+    Vector2 posTitle = {290, 100};
+    int TitleSize = 250;
+    
+    float PlayBtnX = 930;
+    float PlayBtnY = 650;
+
+    char PlayText[5] = "PLAY";
+    Vector2 posPlayText = {760, 580};
+    int PlayTextSize = 150;
+    
+    Vector2 posPlayShdw = {767, 587};
+    
+    float PlayInvisX = 700;
+    float PlayInvisY = 560;
+    Rectangle invisPlayBtn = {PlayInvisX, PlayInvisY, 470, 180};
+    float alphaInvis = 0.0f;
+    
+    
+    // ---------------------- INITIALIZATION ---------------------- //
     const int screenWidth = 1920;
     const int screenHeight = 1080;
+    ToggleBorderlessWindowed();
+    
     InitWindow(screenWidth, screenHeight, "Mastermind");
     SetTargetFPS(60);
+    
     
     //--------------------------------------------------------------------------------------
 
@@ -51,6 +46,18 @@ int main(void)
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
+        Vector2 MousePos = GetMousePosition();
+        bool isHoveringPlayBtn = CheckCollisionPointRec(MousePos, invisPlayBtn);
+        
+        if (isHoveringPlayBtn && IsMouseButtonPressed(MOUSE_LEFT_BUTTON) && mainScreen == true) {
+            mainScreen = false;
+            printf("DEBUG!!!!!!!!!!!!!!!!!!!!!!!");
+            fflush(stdout);
+        } else if (isHoveringPlayBtn) {
+            SetMouseCursor(MOUSE_CURSOR_POINTING_HAND);
+        } else {
+            SetMouseCursor(MOUSE_CURSOR_DEFAULT);
+        }
 
         // Draw
         //----------------------------------------------------------------------------------
@@ -58,9 +65,14 @@ int main(void)
 
             ClearBackground(BLACK);
             
-            if (alphaTitle < 1) alphaTitle += 0.01f;
-            DrawTextEx(font, Title, posTitle, 250, 15, Fade(GOLD, alphaTitle)); // OBS! Order of operators: (Font var, string var, position var, SIZE, KERNING, COLOR);
-            
+            if (mainScreen) {
+                if (alphaMain < 1) alphaMain += 0.01f;
+                DrawTextEx(font, Title, posTitle, TitleSize, 15, Fade(GOLD, alphaMain)); // OBS! Order of operators: (Font var, string var, position var, SIZE, KERNING, COLOR);
+                DrawEllipse(PlayBtnX, PlayBtnY, 250, 100, Fade(GOLD, alphaMain));
+                DrawRectangleRec(invisPlayBtn, Fade(WHITE, alphaInvis));
+                DrawTextEx(font, PlayText, posPlayShdw, PlayTextSize, 8, Fade(BLACK, alphaMain));
+                DrawTextEx(font, PlayText, posPlayText, PlayTextSize, 8, Fade(WHITE, alphaMain));
+            }
 
         EndDrawing();
         //----------------------------------------------------------------------------------
